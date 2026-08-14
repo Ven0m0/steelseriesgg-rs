c3798d82df978e9645c62ed1bd62ae6bc6327105
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-08-15 00:42:44 +0200

    Delete .kilo/plugins directory

--- .kilo/plugins/trim-tool-name.js
+++
@@ -1,9 +0,0 @@
-export const TrimToolNamePlugin = async () => ({
-  "tool.execute.before": async (input) => {
-    if (input.tool && typeof input.tool === "string") {
-      input.tool = input.tool.trim();
-    }
-  },
-});
-
-export default TrimToolNamePlugin;
